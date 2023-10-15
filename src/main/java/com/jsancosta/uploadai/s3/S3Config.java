075c014c17016d42198951157ce7de95d93346c4
Joanã Costa <joanacosta97@hotmail.com>
2023-10-16 00:36:03 +0100

    refactor: Update application.properties
    The S3Config.java class updated to use the values from application.properties

--- src/main/java/com/jsancosta/uploadai/s3/S3Config.java
+++ src/main/java/com/jsancosta/uploadai/s3/S3Config.java
@@ -3,18 +3,25 @@ package com.jsancosta.uploadai.s3;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
-
     @Value("${aws.region}")
     private String awsRegion;
+    @Value("${aws.secretAccessKey}")
+    private String secretAccessKey;
+    @Value("${aws.accessKeyId}")
+    private String accessKeyId;
 
     @Bean
     public S3Client s3Client() {
-        return S3Client.builder().region(Region.of(awsRegion)).build();
+        return S3Client.builder()
+                .region(Region.of(awsRegion))
+                .credentialsProvider(() -> AwsBasicCredentials.create(accessKeyId, secretAccessKey))
+                .build();
     }
 }
 
